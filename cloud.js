160eeabafaddc9e97e7043c71246a43bb47747ec
零度冷咖啡 <sviptzk@163.com>
2020-03-31 21:24:12 +0800

    版本更新

--- cloud.js
+++ cloud.js
@@ -2,34 +2,49 @@ const AV = require('leanengine');
 const mail = require('./utilities/send-mail');
 const Comment = AV.Object.extend('Comment');
 const request = require('request');
-AV.Cloud.afterSave('Comment', function (request) {
-    let currentComment = request.object;
+const spam = require('./utilities/check-spam');
 
-    // 通知站长
-    mail.notice(currentComment);
-    
+function sendNotification(currentComment, defaultIp) {
+    // 发送博主通知邮件
+    if (currentComment.get('mail') !== process.env.BLOGGER_EMAIL) {
+        mail.notice(currentComment);
+    }
+
+    let ip = currentComment.get('ip') || defaultIp;
+    console.log('IP: %s', ip);
+    spam.checkSpam(currentComment, ip);
+
     // AT评论通知
-    let pid =currentComment.get('pid');
+    let rid =currentComment.get('pid') || currentComment.get('rid');
 
-    if (!pid) {
+    if (!rid) {
         console.log("这条评论没有 @ 任何人");
+        return;
+    } else if (currentComment.get('isSpam')) {
+        console.log('检测到垃圾评论，通知邮件取消发送');
         return;
     }
 
-    // 通过被 @ 的评论 id, 则找到这条评论留下的邮箱并发送通知.
     let query = new AV.Query('Comment');
-    query.get(pid).then(function (parentComment) {
-        if (parentComment.get('mail')) {
+    query.get(rid).then(function (parentComment) {
+        if (parentComment.get('mail') && parentComment.get('mail') !== process.env.BLOGGER_EMAIL) {
             mail.send(currentComment, parentComment);
         } else {
-            console.log(currentComment.get('nick') + " @ 了" + parentComment.get('nick') + ", 但被 @ 的人没留邮箱... 无法通知");
+            console.log('被@者匿名，不会发送通知');
         }
+        
     }, function (error) {
-        console.warn('好像 @ 了一个不存在的人!!!');
+        console.warn('获取@对象失败！');
     });
+}
+
+AV.Cloud.afterSave('Comment', function (req) {
+    let currentComment = req.object;
+    // 检查垃圾评论
+    return sendNotification(currentComment, req.meta.remoteAddress);
 });
 
-AV.Cloud.define('resend_mails', function(req) {
+AV.Cloud.define('resend-mails', function(req) {
     let query = new AV.Query(Comment);
     query.greaterThanOrEqualTo('createdAt', new Date(new Date().getTime() - 24*60*60*1000));
     query.notEqualTo('isNotified', true);
@@ -48,10 +63,11 @@ AV.Cloud.define('resend_mails', function(req) {
 
         });
     });
-});
+  });
 
-AV.Cloud.define('self_wake', function(req) {
+AV.Cloud.define('self-wake', function(req) {
     request(process.env.ADMIN_URL, function (error, response, body) {
         console.log('自唤醒任务执行成功，响应状态码为:', response && response.statusCode);
-    });
+      });
 })
+
--- utilities/send-mail.js
+++ utilities/send-mail.js
@@ -1,113 +1,151 @@
-'use strict';
-const nodemailer = require('nodemailer');
-const ejs = require('ejs');
-const fs  = require('fs');
-const path = require('path');
-var request = require('request');
+"use strict";
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const fs = require("fs");
+const path = require("path");
+var request = require("request");
 let config = {
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-    }
-}
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+};
 
 if (process.env.SMTP_SERVICE != null) {
-    config.service = process.env.SMTP_SERVICE;
+  config.service = process.env.SMTP_SERVICE;
 } else {
-    config.host = process.env.SMTP_HOST;
-    config.port = parseInt(process.env.SMTP_PORT);
-    config.secure = process.env.SMTP_SECURE === "false" ? false : true;
+  config.host = process.env.SMTP_HOST;
+  config.port = parseInt(process.env.SMTP_PORT);
+  config.secure = process.env.SMTP_SECURE === "false" ? false : true;
 }
 
 const transporter = nodemailer.createTransport(config);
-let templateName = process.env.TEMPLATE_NAME ?  process.env.TEMPLATE_NAME : "default";
-let noticeTemplate = ejs.compile(fs.readFileSync(path.resolve(process.cwd(), 'template', templateName, 'notice.ejs'), 'utf8'));
-let sendTemplate = ejs.compile(fs.readFileSync(path.resolve(process.cwd(), 'template', templateName, 'send.ejs'), 'utf8'));
+let templateName = process.env.TEMPLATE_NAME
+  ? process.env.TEMPLATE_NAME
+  : "default";
+let noticeTemplate = ejs.compile(
+  fs.readFileSync(
+    path.resolve(process.cwd(), "template", templateName, "notice.ejs"),
+    "utf8"
+  )
+);
+let sendTemplate = ejs.compile(
+  fs.readFileSync(
+    path.resolve(process.cwd(), "template", templateName, "send.ejs"),
+    "utf8"
+  )
+);
 
+transporter.verify(function(error, success) {
+  if (error) {
+    console.log("SMTP邮箱配置异常：", error);
+  }
+  if (success) {
+    console.log("SMTP邮箱配置正常！");
+  }
+});
 
 // 提醒站长
-exports.notice = (comment) => {
-
-    // 站长自己发的评论不需要通知
-    if (comment.get('mail') === process.env.TO_EMAIL 
-        || comment.get('mail') === process.env.SMTP_USER) {
-        return;
+exports.notice = comment => {
+  // 站长自己发的评论不需要通知
+  if (
+    comment.get("mail") === process.env.TO_EMAIL ||
+    comment.get("mail") === process.env.SMTP_USER
+  ) {
+    return;
+  }
+  let describe =
+    "### 您的网站\r\n" +
+    process.env.SITE_NAME +
+    "\r\n ### 评论内容" +
+    "\r\n > " +
+    comment.get("comment") +
+    "\r\n" +
+    "原文地址 👉 " +
+    process.env.SITE_URL +
+    comment.get("url") +
+    "\r\n ### 评论人\r\n" +
+    comment.get("nick") +
+    "(" +
+    comment.get("mail") +
+    ")";
+  request.post(
+    {
+      url: "https://sc.ftqq.com/" + process.env.SCKEY + ".send",
+      form: { text: process.env.SITE_NAME + "来评论啦！", desp: describe }
+    },
+    function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log("server酱发送成功！");
+      }
     }
-	let describe = '### 您的网站\r\n'+process.env.SITE_NAME+'\r\n ### 评论内容'+"\r\n > "+comment.get('comment')+'\r\n'+'原文地址 👉 '+ process.env.SITE_URL + comment.get('url') +'\r\n ### 评论人\r\n'+comment.get('nick')+'('+comment.get('mail')+')'
-	request.post({url:'https://sc.ftqq.com/'+process.env.SCKEY+'.send', form:{text:process.env.SITE_NAME+'来评论啦！',desp:describe}}, function(error, response, body) {
-	if (!error && response.statusCode == 200) {
-		console.log("server酱发送成功！")}
-    });
-    let comment_id = process.env.COMMENT ?  process.env.COMMENT : "#comment";
-    let emailSubject = '👉 咚！「' + process.env.SITE_NAME + '」上有新评论了';
-    let emailContent =  noticeTemplate({
-                            siteName: process.env.SITE_NAME,
-                            siteUrl: process.env.SITE_URL,
-                            name: comment.get('nick'),
-                            text: comment.get('comment'),
-                            url: process.env.SITE_URL + comment.get('url') + comment_id,
-                            mail: comment.get('mail'),
-                        });
+  );
+  let comment_id = process.env.COMMENT ? process.env.COMMENT : "#comment";
+  let emailSubject = "👉 咚！「" + process.env.SITE_NAME + "」上有新评论了";
+  let emailContent = noticeTemplate({
+    siteName: process.env.SITE_NAME,
+    siteUrl: process.env.SITE_URL,
+    name: comment.get("nick"),
+    text: comment.get("comment"),
+    url: process.env.SITE_URL + comment.get("url") + comment_id,
+    mail: comment.get("mail")
+  });
 
-    let mailOptions = {
-        from: '"' + process.env.SENDER_NAME + '" <' + process.env.SMTP_USER + '>',
-        to: process.env.TO_EMAIL ? process.env.TO_EMAIL : process.env.SMTP_USER,
-        subject: emailSubject,
-        html: emailContent
-    };
+  let mailOptions = {
+    from: '"' + process.env.SENDER_NAME + '" <' + process.env.SMTP_USER + ">",
+    to: process.env.TO_EMAIL ? process.env.TO_EMAIL : process.env.SMTP_USER,
+    subject: emailSubject,
+    html: emailContent
+  };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        comment.set('isNotified', true);
-        comment.save();
-        console.log("收到一条评论, 已提醒站长");
-    });
-}
-
-// 发送邮件通知他人
-exports.send = (currentComment, parentComment)=> {
-
-    // 站长被 @ 不需要提醒
-    if (parentComment.get('mail') === process.env.TO_EMAIL 
-        || parentComment.get('mail') === process.env.SMTP_USER) {
-        return;
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
     }
-    let emailSubject = '👉 叮咚！「' + process.env.SITE_NAME + '」上有人@了你';
-    let emailContent = sendTemplate({
-                            siteName: process.env.SITE_NAME,
-                            siteUrl: process.env.SITE_URL,
-                            pname: parentComment.get('nick'),
-                            ptext: parentComment.get('comment'),
-                            name: currentComment.get('nick'),
-                            text: currentComment.get('comment'),
-                            url: process.env.SITE_URL + currentComment.get('url') + "#" + currentComment.get('pid')
-                        });
-    let mailOptions = {
-        from: '"' + process.env.SENDER_NAME + '" <' + process.env.SMTP_USER + '>',
-        to: parentComment.get('mail'),
-        subject: emailSubject,
-        html: emailContent
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        currentComment.set('isNotified', true);
-        currentComment.save();
-        console.log(currentComment.get('nick') + " @了" + parentComment.get('nick') + ", 已通知.");
-    });
+    console.log("收到一条评论, 已提醒站长");
+    comment.set("isNotified", true);
+    comment.save();
+  });
 };
 
-// 该方法可验证 SMTP 是否配置正确
-exports.verify = function(){
-    console.log("....");
-    transporter.verify(function(error, success) {
-        if (error) {
-            console.log(error);
-        }
-        console.log("Server is ready to take our messages");
-    });    
+// 发送邮件通知他人
+exports.send = (currentComment, parentComment) => {
+  // 站长被 @ 不需要提醒
+  if (
+    parentComment.get("mail") === process.env.TO_EMAIL ||
+    parentComment.get("mail") === process.env.SMTP_USER
+  ) {
+    return;
+  }
+  let emailSubject = "👉 叮咚！「" + process.env.SITE_NAME + "」上有人@了你";
+  let comment_id = process.env.COMMENT ? process.env.COMMENT : "#comment";
+  let emailContent = sendTemplate({
+    siteName: process.env.SITE_NAME,
+    siteUrl: process.env.SITE_URL,
+    pname: parentComment.get("nick"),
+    ptext: parentComment.get("comment"),
+    name: currentComment.get("nick"),
+    text: currentComment.get("comment"),
+    url: process.env.SITE_URL + currentComment.get("url") + comment_id
+  });
+  let mailOptions = {
+    from: '"' + process.env.SENDER_NAME + '" <' + process.env.SMTP_USER + ">",
+    to: parentComment.get("mail"),
+    subject: emailSubject,
+    html: emailContent
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(
+      currentComment.get("nick") +
+        " @了" +
+        parentComment.get("nick") +
+        ", 已通知."
+    );
+    currentComment.set("isNotified", true);
+    currentComment.save();
+  });
 };
